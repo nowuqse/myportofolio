eb94962d6948a6a142fd7d91bfa105056e7f5869
nowuqse <cindy.olivia@ui.ac.id>
2026-09-09 21:03:27 +0700

    test: add tests for project page

--- main/tests.py
+++ main/tests.py
@@ -2,8 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
-
+from main.models import Experience, Project
 
 class MainTest(TestCase):
     def setUp(self):
@@ -11,6 +10,12 @@ class MainTest(TestCase):
             title="Kombistek Teaching Assistant",
             description="Assisted the teaching and learning process for Kombistek classes at Fasilkom UI.",
             category="part-time",
+        )
+
+        self.project = Project.objects.create(
+                    title="Personal Portofolio",
+                    description="A personal portfolio website showcasing my skills and experiences.",
+                    category="web",
         )
 
     def test_main_url_is_accessible(self):
@@ -56,3 +61,33 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Finished")
         self.assertNotContains(response, "Ongoing")
+
+    def test_project_url_is_accessible(self):
+        response = self.client.get(reverse("main:show_project"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "project.html")
+        self.assertContains(response, self.project.title)
+
+    def test_project_page(self):
+        response = self.client.get(reverse("main:show_project"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "project.html")
+        self.assertContains(response, self.project.title)
+        self.assertContains(response, self.project.description)
+        self.assertContains(response, "Web Development")
+        self.assertContains(response, "Personal Portofolio")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')    
+
+    def test_empty_project_page(self):
+        Project.objects.all().delete()
+        response = self.client.get(reverse("main:show_project"))
+
+        self.assertContains(response, "No project added.")     
+
+    def test_project_model(self):
+        self.assertEqual(str(self.project), "Personal Portofolio")
+        self.assertEqual(self.project.category, "web")
+
+    
